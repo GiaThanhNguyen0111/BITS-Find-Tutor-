313ac59698e831bd40aca62b2d890abdea7c4b20
DaoAnhVu1 <daoanhvu765@gmail.com>
2023-11-14 23:44:59 +0700

    add card section in homepage

--- frontend/app/(unprotected)/(home)/page.tsx
+++ frontend/app/(unprotected)/(home)/page.tsx
@@ -2,37 +2,31 @@
 import Lottie from "lottie-react";
 import mainAnimation from "../../../animation/main.json";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { motion, Variants } from "framer-motion";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { motion } from "framer-motion";
+import { BookA, GraduationCap, CalendarCheck, Users } from "lucide-react";
 
 const HomePage = () => {
   return (
     <div className="home-container h-3/4 px-20 pb-20">
       {/* Hero Container */}
-      <div className="hero-container flex h-[800px] mb-20">
+      <div className="hero-container flex h-[600px] mb-6 ">
         <motion.div
           className="flex-1 flex flex-col items-center justify-center"
           initial={{ opacity: 0, x: 100 }}
           whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1.5 }}
+          transition={{ duration: 1 }}
           viewport={{ once: true }}
         >
-          <p className="font-bold text-5xl">
+          <p className="font-bold text-4xl">
             Welcome to <span className=" text-emerald-500 uppercase">Logo</span>
             ,
           </p>
-          <p className="font-bold text-5xl text-center px-20">
+          <p className="font-bold text-4xl text-center px-20">
             Discover the ultimate platform for online education search and
             exploration.
           </p>
-          <p className="mt-2 text-2xl">Let's start your learning today.</p>
+          <p className="mt-2 text-xl">Let's start your learning today.</p>
           <div className="mt-5 flex gap-5">
             <Button variant={"secondary"} className="w-48 text-xl">
               Sign up
@@ -40,72 +34,99 @@ const HomePage = () => {
             <Button className="w-48 text-xl bg-emerald-500">Login</Button>
           </div>
         </motion.div>
-        <div className="flex flex-1 items-center justify-center">
+        <motion.div
+          className="flex flex-1 items-center justify-center"
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+        >
           <Lottie animationData={mainAnimation} />
-        </div>
+        </motion.div>
       </div>
 
-      <div className="card-container w-full flex justify-around gap-10 px-20">
+      <div className="card-container w-full flex justify-around gap-10">
         <motion.div
-          className="w-1/3"
+          className="w-1/4 shadow-xl"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
           viewport={{ once: true }}
         >
-          <Card>
+          <Card className="h-72  flex flex-col justify-center">
             <CardHeader>
-              <CardTitle className="text-center">Card Title 1</CardTitle>
-              <CardDescription>Card Description</CardDescription>
+              <CardTitle className="text-center text-xl">
+                1. Choose a subject
+              </CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <div className="flex justify-center mb-5">
+                <BookA className="w-32 h-32" />
+              </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
           </Card>
         </motion.div>
 
         <motion.div
-          className="w-1/3"
+          className="w-1/4  shadow-xl"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, delay: 0.25 }}
           viewport={{ once: true }}
         >
-          <Card>
+          <Card className="h-72  flex flex-col justify-center">
             <CardHeader>
-              <CardTitle className="text-center">Card Title 2</CardTitle>
-              <CardDescription>Card Description</CardDescription>
+              <CardTitle className="text-center text-xl">
+                2. Choose a tutor
+              </CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <div className="flex justify-center mb-5">
+                <GraduationCap className="w-32 h-32" />
+              </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
           </Card>
         </motion.div>
 
         <motion.div
-          className="w-1/3"
+          className="w-1/4  shadow-xl"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, delay: 0.5 }}
           viewport={{ once: true }}
         >
-          <Card>
+          <Card className="h-72  flex flex-col justify-center">
             <CardHeader>
-              <CardTitle className="text-center">Card Title 3</CardTitle>
-              <CardDescription>Card Description</CardDescription>
+              <CardTitle className="text-center text-xl">
+                3. Scheduling
+              </CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <div className="flex justify-center mb-5">
+                <CalendarCheck className="w-32 h-32" />
+              </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
+          </Card>
+        </motion.div>
+
+        <motion.div
+          className="w-1/4  shadow-xl"
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.75 }}
+          viewport={{ once: true }}
+        >
+          <Card className="h-72 flex flex-col justify-center">
+            <CardHeader>
+              <CardTitle className="text-center text-xl">
+                4. Start learning
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex justify-center mb-5">
+                <Users className="w-32 h-32" />
+              </div>
+            </CardContent>
           </Card>
         </motion.div>
       </div>
