37a5b1c225f2bd38468ea413282d5888965e3ba0
DaoAnhVu1 <daoanhvu765@gmail.com>
2023-11-15 08:37:42 +0700

    further homepage

--- frontend/app/(unprotected)/(home)/page.tsx
+++ frontend/app/(unprotected)/(home)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Lottie from "lottie-react";
 import mainAnimation from "../../../animation/main.json";
+import main2Animation from "../../../animation/main2.json";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { motion } from "framer-motion";
@@ -10,7 +11,7 @@ const HomePage = () => {
   return (
     <div className="home-container h-3/4 px-20 pb-20">
       {/* Hero Container */}
-      <div className="hero-container flex h-[600px] mb-6 ">
+      <div className="hero-container flex h-[600px] mb-5 ">
         <motion.div
           className="flex-1 flex flex-col items-center justify-center"
           initial={{ opacity: 0, x: 100 }}
@@ -26,12 +27,17 @@ const HomePage = () => {
             Discover the ultimate platform for online education search and
             exploration.
           </p>
-          <p className="mt-2 text-xl">Let's start your learning today.</p>
+          <p className="mt-2 text-xl">Let&apos;s start your learning today.</p>
           <div className="mt-5 flex gap-5">
-            <Button variant={"secondary"} className="w-48 text-xl">
+            <Button
+              variant={"secondary"}
+              className="w-48 text-xl hover:bg-[#d8d4d6]"
+            >
               Sign up
             </Button>
-            <Button className="w-48 text-xl bg-emerald-500">Login</Button>
+            <Button className="w-48 text-xl bg-emerald-500 hover:bg-[#019c91]">
+              Login
+            </Button>
           </div>
         </motion.div>
         <motion.div
@@ -45,15 +51,15 @@ const HomePage = () => {
         </motion.div>
       </div>
 
-      <div className="card-container w-full flex justify-around gap-10">
+      <div className="card-container w-full flex justify-around gap-10 mb-8">
         <motion.div
-          className="w-1/4 shadow-xl"
+          className="w-1/4 shadow-md"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
           viewport={{ once: true }}
         >
-          <Card className="h-72  flex flex-col justify-center">
+          <Card className="h-72  flex flex-col justify-center hover:scale-105 transition">
             <CardHeader>
               <CardTitle className="text-center text-xl">
                 1. Choose a subject
@@ -68,13 +74,13 @@ const HomePage = () => {
         </motion.div>
 
         <motion.div
-          className="w-1/4  shadow-xl"
+          className="w-1/4  shadow-md"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, delay: 0.25 }}
           viewport={{ once: true }}
         >
-          <Card className="h-72  flex flex-col justify-center">
+          <Card className="h-72  flex flex-col justify-center hover:scale-105 transition">
             <CardHeader>
               <CardTitle className="text-center text-xl">
                 2. Choose a tutor
@@ -89,13 +95,13 @@ const HomePage = () => {
         </motion.div>
 
         <motion.div
-          className="w-1/4  shadow-xl"
+          className="w-1/4  shadow-md"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, delay: 0.5 }}
           viewport={{ once: true }}
         >
-          <Card className="h-72  flex flex-col justify-center">
+          <Card className="h-72  flex flex-col justify-center hover:scale-105 transition">
             <CardHeader>
               <CardTitle className="text-center text-xl">
                 3. Scheduling
@@ -110,13 +116,13 @@ const HomePage = () => {
         </motion.div>
 
         <motion.div
-          className="w-1/4  shadow-xl"
+          className="w-1/4  shadow-md"
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, delay: 0.75 }}
           viewport={{ once: true }}
         >
-          <Card className="h-72 flex flex-col justify-center">
+          <Card className="h-72 flex flex-col justify-center hover:scale-105 transition">
             <CardHeader>
               <CardTitle className="text-center text-xl">
                 4. Start learning
@@ -130,6 +136,28 @@ const HomePage = () => {
           </Card>
         </motion.div>
       </div>
+
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 0.25 }}
+        viewport={{ once: true }}
+        className="why-container w-full flex px-20 mb-8"
+      >
+        <div className="flex-1 flex items-center justify-center">
+          <Lottie animationData={main2Animation} />
+        </div>
+        <div className="flex-1 flex items-center justify-center flex-col ">
+          <p className="font-bold text-3xl">What sets us apart?</p>
+          <p className="text-xl mt-2">
+            At <span className="text-emerald-500 uppercase font-semibold">Logo</span>, we are committed
+            to revolutionizing online education. Here&apos;s what sets us apart:
+          </p>
+          <p>
+            
+          </p>
+        </div>
+      </motion.div>
     </div>
   );
 };
