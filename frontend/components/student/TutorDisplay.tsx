e23d02e83197dd9b0d92a5a1d826699511e5914d
dbaonguyen <baond39@gmail.com>
2023-11-23 12:22:43 +0700

    Upate card display demo student view

--- frontend/components/student/TutorDisplay.tsx
+++ frontend/components/student/TutorDisplay.tsx
@@ -1,5 +1,48 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 const TutorDisplay = () => {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-5 mt-5 ml-5">
+        <Card className="h-64 w-full flex flex-col justify-center cursor-pointer">
+          <CardHeader>
+            <CardTitle className="text-center text-xl">Teacher Ba </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+        <Card className="h-64 w-full flex flex-col justify-center cursor-pointer">
+          <CardHeader>
+            <CardTitle className="text-center text-xl">Teacher Ba </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+        <Card className="h-64 w-full flex flex-col justify-center cursor-pointer">
+          <CardHeader>
+            <CardTitle className="text-center text-xl">Teacher Ba </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+        <Card className="h-64 w-full flex flex-col justify-center cursor-pointer">
+          <CardHeader>
+            <CardTitle className="text-center text-xl">Teacher Ba </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+        <Card className="h-64 w-full flex flex-col justify-center cursor-pointer">
+          <CardHeader>
+            <CardTitle className="text-center text-xl">Teacher Ba </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+        <Card className="h-64 w-full flex flex-col justify-center cursor-pointer">
+          <CardHeader>
+            <CardTitle className="text-center text-xl">Teacher Ba </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default TutorDisplay;
